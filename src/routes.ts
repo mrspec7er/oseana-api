c6610f7e8e0606009102bc0f2baf8e199151a5c5
mrspec7er <wijayakusumasandi@gmail.com>
2023-07-30 21:56:06 +0800

    feat: 🎸 route
    added welcome message

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,11 @@ import ticket from "./modules/ticket/ticket.route";
 import cart from "./modules/cart/cart.route";
 
 function routes(app: Express) {
+  app.use("/", (req, res) => {
+    res.status(200).json({
+      message: "Hello there!",
+    });
+  });
   app.use("/api/v1", user);
   app.use("/api/v1", content);
   app.use("/api/v1", ticket);
