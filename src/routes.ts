8864af8486972937fe0bedc6c3bd48dd22f7f868
mrspec7er <wijayakusumasandi@gmail.com>
2023-07-31 22:46:30 +0800

    fix: 🐛 route
    fixed testing handler position

--- src/routes.ts
+++ src/routes.ts
@@ -6,15 +6,15 @@ import ticket from "./modules/ticket/ticket.route";
 import cart from "./modules/cart/cart.route";
 
 function routes(app: Express) {
+  app.use("/api/v1", user);
+  app.use("/api/v1", content);
+  app.use("/api/v1", ticket);
+  app.use("/api/v1", cart);
   app.use("/", (req, res) => {
     res.status(200).json({
       message: "Hello there!",
     });
   });
-  app.use("/api/v1", user);
-  app.use("/api/v1", content);
-  app.use("/api/v1", ticket);
-  app.use("/api/v1", cart);
 }
 
 export default routes;
